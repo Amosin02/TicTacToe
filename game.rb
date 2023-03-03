5e3f4919d58f225ae4aa141c4397c7e1c54b2620
Nhoj <amosinnhoj@gmail.com>
2023-03-03 13:21:47 +0800

    Add the checking of rows and column. Add also a way to restart the game when theres a winner

--- game.rb
+++ game.rb
@@ -1,12 +1,15 @@
 class TicTacToe
-    @@sym1 = 'N'
-    @@sym2 = 'B'
-    @@name = 'Nhoj'
-    @@name2 = 'Ber'
-    @@check_robot = ['1','2','3','4','5','6','7','8','9']
+    def initialize()
+        @@sym1 = 'N'
+        @@sym2 = 'B'
+        @@name = 'Nhoj'
+        @@name2 = 'Ber'
+        @@check_robot = ['1','2','3','4','5','6','7','8','9']
+    end
+
     def start_game()
         #ask_symbol()
-        #print_board()
+        print_board()
         ask_move()
     end
 
@@ -23,19 +26,12 @@ class TicTacToe
     end
 
     def print_board() #add a way to update the board
-        position = [1,2,3,4,5,6,7,8,9]
-        #@@check_robot.each_with_index do |x,ind|
-        #    if x == @sym1
-        #        position[ind] = @@sym1
-        #    end
-        #end
-        #p position
         puts """
-         #{position[0]} | #{position[1]} | #{position[2]}
+         #{@@check_robot[0]} | #{@@check_robot[1]} | #{@@check_robot[2]}
         ---+---+---
-         4 | 5 | 6
+         #{@@check_robot[3]} | #{@@check_robot[4]} | #{@@check_robot[5]}
         ---+---+---
-         7 | 8 | 9
+         #{@@check_robot[6]} | #{@@check_robot[7]} | #{@@check_robot[8]}
         ---+---+---\n"""
     end
 
@@ -49,21 +45,64 @@ class TicTacToe
 
     def ask_move()
         x = 0
-        while x < 3
+        while x < 9
             puts "#{@@name} please enter a number (1-9) that is available to place '#{@@sym1}'"
             move = gets.chomp #chomp is fucking important
             update_check(move,@@sym1)
-            p @@check_robot
-            puts "#{@@name2} please enter a number (1-9) that is available to place '#{@@sym2}'"
-        
+            print_board()
+            find_winner()
+
+            puts "#{@@name2} please enter a number (1-9) that is available to place '#{@@sym2}'"            
             move = gets.chomp
             update_check(move,@@sym2)
-            p @@check_robot
             x+= 1
             print_board()
+            find_winner()
         end
-        #ilagay yung mga tira sa isang array then use that array to check the 
-        #winner and use that to update the board
+    end
+
+    def find_winner()
+        result = @@check_robot.each_slice(3).to_a
+        hold1 = []; hold2 = []; hold3 = []
+        result.each do |x|
+            hold1 << x[0]
+            hold2 << x[1]
+            hold3 << x[2]
+
+            #check the rows
+            if x.uniq.length == 1
+                puts "Game Over! #{x[0]} won"
+                ask_new_game()
+            end
+        end
+
+        #checks the column
+       if hold1.uniq.length == 1 
+        puts "Game Over! #{hold1[0]} won"
+        ask_new_game()
+       end
+       if hold2.uniq.length == 1 
+        puts "Game Over! #{hold2[0]} won"
+        ask_new_game()
+       end
+       if hold3.uniq.length == 1 
+        puts "Game Over! #{hold3[0]} won"
+        ask_new_game()
+       end
+        
+    end
+
+    def ask_new_game()
+        puts "Would you like to play a new game? press 'y' for Yes or 'n' for No."
+        continue = gets.chomp
+       if continue == 'y'
+        initialize()
+        start_game()
+       elsif continue == 'n'
+        exit
+       else
+        puts "Type 'y' for Yes or 'n' for No"
+       end
     end
 end
 
